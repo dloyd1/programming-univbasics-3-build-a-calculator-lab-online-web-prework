ea8db5a9c56a23ab4303c999d021cd269db4f08d
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 07:31:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,22 +7,18 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  value_div = num1 / num2
-  value_div
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  value_prod = num1 * num2
-  value_prod
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  value_mod = num1.remainder(num2)
-  value_mod
+  num1.remainder(num2)
 end
 
 def square_root(num)
-  value_sqrt = Math.sqrt(num)
-  value_sqrt
+  Math.sqrt(num)
 end
 
